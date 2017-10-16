78c17ac3a51057a4f063a4ed0abc79bf1a2da06e
Batuhan <bapaydin67@gmail.com>
2017-10-16 16:45:35 +0300

    defer added

--- defer/defer.go
+++ defer/defer.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"os"
+	"fmt"
+)
+
 func main() {
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(s string) *os.File {
+	fmt.Println("Creating")
+	file, err := os.Create(s)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func writeFile(file *os.File) {
+	fmt.Println("wiriting")
+	fmt.Fprintln(file, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	f.Close()
 }
